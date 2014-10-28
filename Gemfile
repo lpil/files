eadd18ca90ac694290efd657c9a1b7d7b9b97499
Louis Pilfold <louis@lpil.uk>
2014-10-28 19:39:16 +0000

    User controller tests

--- Gemfile
+++ Gemfile
@@ -16,7 +16,6 @@ gem 'turbolinks'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
 
-gem 'spring',        group: :development
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
@@ -26,6 +25,18 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
+group :test do
+  gem 'capybara'
+  gem 'minitest'
+  gem 'minitest-rails'
+  gem 'minitest-rails-capybara'
+end
+
+group :development do
+  gem 'rubocop'
+  gem 'spring'
+end
 
 # Use the pry REPL for debugging
 gem 'pry', group: [:development, :test]
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,41 +5,52 @@ class UsersControllerTest < ActionController::TestCase
     @user = users(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create user" do
+  test 'should create user' do
     assert_difference('User.count') do
-      post :create, user: { email: @user.email, name: @user.name, password_digest: @user.password_digest, project_starter: @user.project_starter }
+      post :create, user: {
+        email: 'new@user.com',
+        name: 'Another ' + @user.name,
+        password: default_password,
+        password_confirmation: default_password,
+        project_starter: @user.project_starter
+      }
     end
 
     assert_redirected_to user_path(assigns(:user))
   end
 
-  test "should show user" do
+  test 'should show user' do
     get :show, id: @user
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @user
     assert_response :success
   end
 
-  test "should update user" do
-    patch :update, id: @user, user: { email: @user.email, name: @user.name, password_digest: @user.password_digest, project_starter: @user.project_starter }
+  test 'should update user' do
+    patch :update, id: @user, user: {
+      email: @user.email,
+      name: @user.name,
+      password_digest: @user.password_digest,
+      project_starter: @user.project_starter 
+    }
     assert_redirected_to user_path(assigns(:user))
   end
 
-  test "should destroy user" do
+  test 'should destroy user' do
     assert_difference('User.count', -1) do
       delete :destroy, id: @user
     end
