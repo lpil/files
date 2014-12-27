ffb64bfd47ecaf52eecc97ee5e040ca9ae4e6290
Louis Pilfold <louis@lpil.uk>
2014-12-27 16:53:52 +0000

    Remove coffeescript

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module Files
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Let's not use coffeescript
+    config.generators do |g|
+      g.javascript_engine :js
+    end
   end
 end
